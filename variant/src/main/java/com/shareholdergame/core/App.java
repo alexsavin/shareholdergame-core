53a0ba03cf61d5c090646b59b83132e7b4703a83
alexsavin <savin.aleksander@gmail.com>
2018-01-28 20:10:32 +0300

    Base structure of the project

---
+++ variant/src/main/java/com/shareholdergame/core/App.java
@@ -0,0 +1,13 @@
+package com.shareholdergame.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
