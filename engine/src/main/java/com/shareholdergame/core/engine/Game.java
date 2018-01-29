d68fc56c18976fb26510e659eec9f52163f6ef77
Aliaksandr Savin <aliaksandr_savin@epam.com>
2018-01-29 18:45:51 +0300

    Draft version

--- engine/src/main/java/com/shareholdergame/core/engine/Game.java
+++ engine/src/main/java/com/shareholdergame/core/engine/Game.java
@@ -1,7 +1,20 @@
 package com.shareholdergame.core.engine;
 
+import com.shareholdergame.core.variant.GameVariant;
+
 /**
+ * This class represents a game.
+ * Steps
+ * 1 Initialization
+ * 2 Moves
+ * 3 Results calculation
+ *
+ *
  * Created by alex on 29.1.18.
  */
 public class Game {
+
+    public static Game initialize(GameVariant gameVariant, int playerCount) {
+        return null;
+    }
 }
