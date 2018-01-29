8e6f37e3d32e6a51a3e7a742059fd4810d9a9f28
alexsavin <savin.aleksander@gmail.com>
2018-01-29 09:59:17 +0300

    Draft version of modules

---
+++ engine/src/main/java/com/shareholdergame/core/engine/Cash.java
@@ -0,0 +1,7 @@
+package com.shareholdergame.core.engine;
+
+/**
+ * Created by alex on 29.1.18.
+ */
+public class Cash {
+}
---
+++ variant/src/main/java/com/shareholdergame/core/variant/GameVariant.java
@@ -0,0 +1,7 @@
+package com.shareholdergame.core.variant;
+
+/**
+ * Created by alex on 29.1.18.
+ */
+public class GameVariant {
+}
